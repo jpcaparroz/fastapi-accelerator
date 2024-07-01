8199d713a5111491e56488df54f0628a875264b1
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-07-01 19:38:50 -0300

    New template + fix imports

--- src/api/v1/data/template/user_template.py
+++ src/api/v1/data/template/user_template.py
@@ -1,13 +1,28 @@
 from fastapi import Body
 
-from schemas import user_schema as schemas
+from schemas.user_schema import CreateUserSchema
+from schemas.user_schema import UpdateUserSchema
 
 
 UpdateUserBody = Body(
     title='User',
-    description='The user json representation.',
+    description='The update user json representation.',
     examples=[
-        schemas.UpdateUserSchema(
+        UpdateUserSchema(
+            user_name='user_example',
+            user_password='******',
+            user_email='example@gmail.com',
+            is_admin=True
+        )
+    ]
+)
+
+
+CreateUserBody = Body(
+    title='User',
+    description='The create user json representation.',
+    examples=[
+        CreateUserSchema(
             user_name='user_example',
             user_password='******',
             user_email='example@gmail.com',
