5efdf84c5f0aa7801e15a4f3ad92838300631f84
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-07-09 11:52:45 -0300

    Import new lib + override env variables

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
 
+from strtobool import strtobool
+
 from dotenv import load_dotenv
 
 
 ENV_PATH = Path(f'{os.getcwd()}/.env')
-load_dotenv(dotenv_path=ENV_PATH)
+load_dotenv(dotenv_path=ENV_PATH, override=True)
 
 
 def get_env(env_name: str) -> str:
@@ -30,7 +32,7 @@ def get_env_fastapi_config() -> dict:
         "host": os.getenv('FAST_API_HOST'),
         "port": int(os.getenv('FAST_API_PORT')),
         "log_level": os.getenv('FAST_API_LOG_LEVEL'),
-        "reload": os.getenv('FAST_API_RELOAD'),
+        "reload": strtobool(os.getenv('FAST_API_RELOAD')),
         "workers": int(os.getenv('FAST_API_WORKERS'))
     }
     return config
